25e50d308ca3c35628f55dea45bd33d3e8fb6c88
dudubouneto <eduardobouhid.neto@gmail.com>
2023-06-02 19:46:31 -0300

    feat: 'shorten' endpoint added

--- src/urlshortener/core.clj
+++ src/urlshortener/core.clj
@@ -5,9 +5,10 @@
             [ring.middleware.defaults :refer :all]
             [clojure.pprint :as pp]
             [clojure.string :as str]
-            [clojure.data.json :as json])
+            [clojure.data.json :as json]
+            [clojure.set :as set])
   (:gen-class))
-
+;; (load-file "src/shortener.clj")
 
 ; Simple Body Page
 (defn simple-body-page [req]
@@ -31,15 +32,56 @@
                 (pp/pprint req)
                 (str "Hello " (:name (:params req))))})
 
+  ; hash-id
+  (defn reverse-str
+  [my-str]
+  (apply str (reverse my-str)))
+
+  (defn hash-id
+  [n]
+  (let [symbolmap (zipmap (concat
+                           (map char (range 48 58))
+                           (map char (range 97 123))
+                           (map char (range 65 91)))
+                          (range 62))]
+    (loop [decNumber n
+           result []]
+      (if (= decNumber 0)
+        (reverse-str result)
+        (recur (quot decNumber 62)
+               (conj result ((set/map-invert symbolmap) (mod decNumber 62))))))))
+
+  ;; gen-id
+  (defn gen-id [req]
+  (rand-int 350000000))
+
+  (defn shorten [req]
+    {
+      :status 200
+      :headers {"Content-Type" "text/html"}
+      :body    (str (hash-id (gen-id req)))
+    }
+  )
+
+  ;; atom manipulation functions
+  (defn store-url [id url]
+  (swap! url-map assoc id url))
+
+  (defn get-url [id]
+  (get @url-map id))
+
   (defroutes app-routes
   (GET "/" [] simple-body-page)
   (GET "/request" [] request-example)
+  (GET "/shorten" [] shorten)
   (GET "/eae" [] hello-name)
+  ;; (GET "/redirect" [] )
   (route/not-found "Error, page not found!"))
 
 (defn -main
   "This is our main entry point"
   [& args]
+  (def url-map (atom {}))
   (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
     ; Run the server with Ring.defaults middleware
     (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
